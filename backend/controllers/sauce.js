ad47837a64e67cc4a50e08018d863481f4e7572a
fred-fittywebdev <contact@fittywebdev.fr>
2022-02-09 08:07:33 +0100

    modifications sur quelques fichiers

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -12,50 +12,50 @@ exports.createSauce = (req, res, next) => {
         dislikes: 0,
         usersLiked: [],
         usersDisliked: []
-        });
+    });
     sauce.save()
-        .then(()=> res.status(201).json({message: 'Sauce enregistré !'}))
-        .catch(error => res.status(400).json({error}));
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        .catch(error => res.status(400).json({ error }));
 };
 
 //modification d'une sauce
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
-    {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body};
-    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
-        .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
+        {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
         .catch(error => res.status(400).json({ error }));
 };
 
 //suppression de la sauce
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-    .then(sauce => {
-        const filename = sauce.imageUrl.split('/images/')[1];
-        fs.unlink(`images/${filename}`, () => {
-            Sauce.deleteOne({ _id: req.params.id })
-                .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
-                .catch(error => res.status(400).json({ error }));
-        });
-    })
-    .catch(error => res.status(500).json({ error }));
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 //présente seulement une sauce
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({_id: req.params.id})
+    Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({error}));
+        .catch(error => res.status(404).json({ error }));
 };
 
 //présente toutes les sauces disponible
 exports.getAllSauce = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({error}));
+        .catch(error => res.status(400).json({ error }));
 };
 
 //gestion des likes et dislikes
@@ -67,14 +67,14 @@ exports.likeSauce = (req, res) => {
     switch (likeState) {
         //si like=1 on incrémente l'attribut likes de la sauce et on ajoute l'id de l'utilisateur dans le tableau usersLiked
         case 1:
-            Sauce.updateOne({_id: sauceId}, {$inc: {likes: 1}, $push: {usersLiked: userId}})
-                .then(() => res.status(200).json({message: "+1 like ajouté à la sauce" }))
-                .catch((error) => res.status(400).json({error}));
+            Sauce.updateOne({ _id: sauceId }, { $inc: { likes: 1 }, $push: { usersLiked: userId } })
+                .then(() => res.status(200).json({ message: "+1 like ajouté à la sauce" }))
+                .catch((error) => res.status(400).json({ error }));
             break;
-            //si like=0 alors on étudie les deux tableaux usersLiked, usersDisliked et on met à jour les attributs likes et dislikes ainsi que les tableaux eux meme selon la présence de l'userId dans l'un des deux
+        //si like=0 alors on étudie les deux tableaux usersLiked, usersDisliked et on met à jour les attributs likes et dislikes ainsi que les tableaux eux meme selon la présence de l'userId dans l'un des deux
         case 0:
             //retourne le tableau correspondant a sauceId
-            Sauce.findOne({_id: sauceId})
+            Sauce.findOne({ _id: sauceId })
                 .then(sauce => {
                     if (sauce.usersLiked.includes(userId)) {
                         //décrémente l'attribut likes de la sauce et supprime l'userId du tableau usersLiked
@@ -83,17 +83,17 @@ exports.likeSauce = (req, res) => {
                             .catch(error => res.status(400).json({ error }));
                     } else if (sauce.usersDisliked.includes(userId)) {
                         //décrémente l'attribut dislikes de la sauce et supprime l'userId du tableau usersDisliked
-                        Sauce.updateOne({_id: sauceId}, {$inc: {dislikes: -1}, $pull: {usersDisliked: userId}})
-                            .then(() => res.status(200).json({message: "Vous avez enlevé votre dislike !"}))
-                            .catch(error => res.status(400).json({error}));
+                        Sauce.updateOne({ _id: sauceId }, { $inc: { dislikes: -1 }, $pull: { usersDisliked: userId } })
+                            .then(() => res.status(200).json({ message: "Vous avez enlevé votre dislike !" }))
+                            .catch(error => res.status(400).json({ error }));
                     }
                 })
-                .catch(error => res.status(400).json({error}));
+                .catch(error => res.status(400).json({ error }));
             break;
-            //si like=-1 on incrémente l'attribut dislikes de la sauce et on ajoute l'id de l'utilisateur dans le tableau usersDisliked
+        //si like=-1 on incrémente l'attribut dislikes de la sauce et on ajoute l'id de l'utilisateur dans le tableau usersDisliked
         case -1:
-            Sauce.updateOne({_id: sauceId}, { $inc: {dislikes: 1}, $push: {usersDisliked: userId}})
-                .then(() => res.status(200).json({message: "+1 dislike ajouté à la sauce" }))
+            Sauce.updateOne({ _id: sauceId }, { $inc: { dislikes: 1 }, $push: { usersDisliked: userId } })
+                .then(() => res.status(200).json({ message: "+1 dislike ajouté à la sauce" }))
                 .catch((error) => res.status(400).json({ error }));
             break;
     }
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,13 +5,13 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.TOKEN);
         const userId = decodedToken.userId;
-        req.auth = {userId};
+        req.auth = { userId };
         if (req.body.userId && req.body.userId !== userId) {
-            throw 'User ID non valable !';
+            throw 'User ID non valide !';
         } else {
             next();
         }
     } catch (error) {
-        res.status(403).json({error: error | 'Requête non authentifiée ! '});
+        res.status(403).json({ error: error | 'Requête non authentifiée ! ' });
     }
 };
