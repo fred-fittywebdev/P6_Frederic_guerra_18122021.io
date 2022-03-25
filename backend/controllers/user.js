65058e8a6b95afd3d5f854ea410b9c152f8d06c2
fred-fittywebdev <contact@fittywebdev.fr>
2022-03-25 19:49:10 +0100

    Fix some bug on unique email error message not diplaying on the fronend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -13,34 +13,34 @@ exports.signup = (req, res, next) => {
                 password: hash
             });
             user.save()
-                .then(() => res.status(201).json({message: 'Utilisateur crée !'}))
-                .catch(error => res.status(400).json({error}));
+                .then(() => res.status(201).json({ message: 'Utilisateur crée !' }))
+                .catch(error => res.status(400).json({ message: error.message }));
         })
-        .catch(error => res.status(500).json({error}));
+        .catch(error => res.status(500).json({ error }));
 };
 
 // Se connecter à un compte utilisateur
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
-        .then(user =>{
-            if(!user){
-                return res.status(401).json({message: 'Utilisateur non trouvé !'});
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Utilisateur non trouvé !' });
             }
             bcrypt.compare(req.body.password, user.password)
-            .then(valid => {
-                if(!valid){
-                    return res.status(401).json({message: 'Mot de passe incorrect !'});
-                }
-                res.status(200).json({
-                    userId: user._id,
-                    token: jwt.sign(
-                        {userId: user._id},
-                        process.env.TOKEN,
-                        {expiresIn: '24h'}
-                    )
-                });
-            })
-            .catch(error => res.status(500).json({error}));
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Mot de passe incorrect !' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            process.env.TOKEN,
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({error}));
+        .catch(error => res.status(500).json({ error }));
 };
